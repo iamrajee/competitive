de31165b6dbb53779be37473ee6fca71155d54a7
Rajendra Singh <singh.raj1997@gmail.com>
2019-05-22 12:37:44 +0530

    wrong ans

--- uva/ch02_dataStructure/11926Multitasking/a.cpp
+++ uva/ch02_dataStructure/11926Multitasking/a.cpp
@@ -5,85 +5,117 @@ int n,m,i,j,J1,J2,k,t1,t1_,t2,t2_,r,r_;
 vector <pair<int,int>> one;
 vector <tuple<int,int,int>> rep;
 int flag;
+int counter=0;
 int main()
 {   cin>>n>>m;
     while(n||m){
         flag = 0;
         one.clear();
         rep.clear();
-        cout<<"****"<<n<<" "<<m<<"****"<<endl;
+        // cout<<++counter<<"****"<<n<<" "<<m<<"****"<<endl;
         for(i=0; i<n; i++){
             cin>>t1>>t2;
+
+            if(flag){
+                continue;
+            }
 
             for(auto it:one){
                 t1_ = it.first;
                 t2_ = it.second;
-                cout<<"=>"<<t1_<<" "<<t2_<<endl;
                 if( (t1_<t1&&t1<t2_) || (t1_<t2&&t2<t2_) ){
+                    // cout<<"=>"<<t1_<<" "<<t2_<<endl;
+                    flag=1;
+                    break;
+                }else if((t1<t1_&&t1_<t2) || (t1<t2_&&t2_<t2)){
                     flag=1;
                     break;
                 }
             }
             if(flag){
                 cout<<"CONFLICT"<<endl;
-                break;
+                continue;
             }
-            cout<<t1<<" "<<t2<<endl;
+            // cout<<t1<<" "<<t2<<endl;
             one.push_back({t1,t2});
         }
-        if(flag){
-            cin>>n>>m;
-            continue;
-        }
+        // if(flag){
+        //     cin>>n>>m;
+        //     continue;
+        // }
         for(i=0; i<m; i++){
             cin>>t1>>t2>>r;
+
+            if(flag){
+                continue;
+            }
 
             for(auto it:one){//multi checked with one
                 t1_ = it.first;
                 t2_ = it.second;
-                J1 = (t1_ - t1)/r+1;
-                J2 = (t1_ - t2)/r+1;
-                cout<<"=>"<<t1_<<" "<<t2_<<endl;
+                if(t2_<t1){
+                    continue;
+                }
+                J1 = ((float)(t1_ - t1)/r<0)?0:(t1_ - t1)/r+1;
+                J2 = ((float)(t1_ - t2)/r<0)?0:(t1_ - t1)/r+1;
+                // cout<<"==>"<<t1_<<" "<<t1+J1*r<<" "<<t2+J2*r<<" "<<t2_<<endl;
                 if( (t1_<t1+J1*r&&t1+J1*r<t2_) || (t1_<t2+J2*r&&t2+J2*r<t2_) ){
+                    // cout<<"==>"<<t1_<<" "<<t1+J1*r<<" "<<t2+J2*r<<" "<<t2_<<endl;
+                    // cout<<"==>"<<t1_<<" "<<t2_<<endl;
                     flag=1;
                     break;
+                }else if(J1>0){
+                    J1--;
+                    if(t1+J1*r<t1_ && t1_<t2+J1*r){
+                        flag=1;
+                        break;
+                    }
                 }
             }
             if(flag){
                 cout<<"CONFLICT"<<endl;
-                break;
+                continue;
             }
 
             for(auto it:rep){//multi checked with multi
                 t1_ = get<0>(it);
                 t2_ = get<1>(it);
                 r_ = get<1>(it);
-
-                if(t1<t1_){
-                    J1 = (t1_ - t1)/r;
-                    // t1+J1*r
-                }else if(t1>t1_){
-                    J2 = (t1 - t1_)/r_;
-                    // t_+J2*r_
-                }else{
-                    flag=1;
-                    break;
+                // cout<<"===>"<<t1_<<" "<<t2_<<" "<<r_<<endl;
+                while(t2_<1000000){
+                    if(t1>t2_){
+                        t1_ +=r_;
+                        t2_ +=r_;
+                        continue;
+                    }
+                    J1 = ((float)(t1_ - t1)/r<0)?0:(t1_ - t1)/r+1;
+                    J2 = ((float)(t1_ - t2)/r<0)?0:(t1_ - t1)/r+1;
+                    
+                    // cout<<"=>"<<t1_<<" "<<t2_<<endl;
+                    // cout<<"====>"<<t1_<<" "<<J1<<" "<<J2<<" "<<t2_<<endl;
+                    if( (t1_<t1+J1*r&&t1+J1*r<t2_) || (t1_<t2+J2*r&&t2+J2*r<t2_) ){
+                        flag=1;
+                        break;
+                    }else if(J1>0){
+                        J1--;
+                        if(t1+J1*r<t1_ && t1_<t2+J1*r){
+                            flag=1;
+                            break;
+                        }
+                    }
+                    t1_ +=r_;
+                    t2_ +=r_;
                 }
-
-                J1 = (t1_ - t1)/r+1;
-                J2 = (t1_ - t2)/r+1;
-                cout<<"=>"<<t1_<<" "<<t2_<<endl;
-                if( (t1_<t1+J1*r&&t1+J1*r<t2_) || (t1_<t2+J2*r&&t2+J2*r<t2_) ){
-                    flag=1;
+                if(flag){
                     break;
                 }
             }
             if(flag){
                 cout<<"CONFLICT"<<endl;
-                break;
+                continue;
             }
 
-            cout<<t1<<" "<<t2<<" "<<r<<endl;
+            // cout<<t1<<" "<<t2<<" "<<r<<endl;
             rep.push_back(make_tuple(t1,t2,r));
         }
 
