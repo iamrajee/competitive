34d60513ad17e9a7ff925543714b2a04a22ca8aa
Rajendra Singh <singh.raj1997@gmail.com>
2019-06-08 13:26:58 +0530

    updated

--- codeforces/competitions/CodeforcesRound#564div2/c/a.cpp
+++ codeforces/competitions/CodeforcesRound#564div2/c/a.cpp
@@ -1,27 +1,79 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int n,i,j,w,t,k,a;
-int x,l,r,sd;
+int n,i,t;
+int flag1,flag2;
+long int zeroes;
 vector <int> v;
-vector <int> v2;
-// bool sortfn(const int & a, const int & b){   
-//     return (a > b);
-// }
+set <int> s;
+map <int,int> m;
+
 int main(){
     cin>>n;
-    cout<<n<<endl;
+    // cout<<n<<endl;
     for(i=0;i<n;i++){
         cin>>t;
-        cout<<t<<" ";
+        // cout<<t<<" ";
+        s.insert(t);
+    }
+    // cout<<endl;
+    for(i=0;i<n;i++){
+        cin>>t;
+        // cout<<t<<" ";
+        m[t] = i;
         v.push_back(t);
     }
-    cout<<endl;
-    for(i=0;i<n;i++){
-        cin>>t;
-        cout<<t<<" ";
-        v2.push_back(t);
+    // cout<<endl;
+
+    zeroes=0;
+    flag1 = 0;
+    flag2 = 0;
+    if(*s.find(1)!=1){
+        int prev = v[n-1]+1;
+        i = n-1;
+        flag1 = 0;
+        while(v[i]==prev-1){
+            zeroes--;
+            if(v[i]==1){
+                flag1=1;
+                break;
+            }
+            i--;
+        }
+        flag2=1;
+        if(flag1==1){
+            // zeroes=0;
+            if(*s.find(v[n-1]+1)==v[n-1]+1){
+                for(i=v[n-1]+1+1;i<n+1;i++){
+                    if(*s.find(i)!=i){
+                        if(m[i]>i-2+zeroes+1){
+                            flag2=0;
+                            break;
+                            // zeroes++;
+                        }
+                    }
+                }
+            }else{
+                flag2=0;
+            }
+        }
     }
-    cout<<endl;
+
+    if(flag1==1 && flag1==1){
+        cout<<n+zeroes<<endl;
+    }else{
+        zeroes=0;
+        if(*s.find(1)!=1){
+            zeroes=m[1]+1;
+        }
+        for(i=1+1;i<n+1;i++){
+            if(*s.find(i)!=i){
+                if(m[i]>i-2+zeroes){
+                    zeroes++;
+                }
+            }
+        }
+        cout<<zeroes+n<<endl;
+    }
     return 0;
 }
--- codeforces/competitions/CodeforcesRound#564div2/d/a.cpp
+++ codeforces/competitions/CodeforcesRound#564div2/d/a.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int n,i,p,c;
 long long int result=1;
 vector <int> d(200000,-1);
+vector <int>::iterator itr;
 int fact(int t_){
     if(d[t_] != -1){
         return d[t_];
@@ -12,6 +13,9 @@ int fact(int t_){
             d[t_] = 1;
             return d[t_];
         }else if(t_==1){
+            d[t_] = 2;
+            return d[t_];
+        }else if(t_==2){
             d[t_] = 2;
             return d[t_];
         }else{
@@ -37,16 +41,16 @@ int main(){
     cin>>n;
     // cout<<n<<endl;
     vector <int> v(n+1,0) ;
-    cout<<" --"<<v.size()<<endl;
-    for(i=0;i<n;i++){
+    for(i=0;i<n-1;i++){
         cin>>p>>c;
         // cout<<p<<" "<<c<<endl;
         v[p]++;
     }
-    for(auto it:v){
-        result*=fact(it);
+    for(itr=++v.begin();itr!=v.end();itr++){
+        result*=fact(*itr);
+        // cout<<"=>"<<*itr<<" , "<<result<<endl;
     }
     result*=n;
-    cout<<result<<endl;
+    cout<<result%998244353<<endl;
     return 0;
 }
