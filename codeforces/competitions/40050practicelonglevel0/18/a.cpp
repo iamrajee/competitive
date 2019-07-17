8bab2a54da7514522db8e86b10ba5f066cd0b806
Rajendra Singh <singh.raj1997@gmail.com>
2019-07-17 10:21:11 +0530

    updated

--- codeforces/competitions/40050practicelonglevel0/18/a.cpp
+++ codeforces/competitions/40050practicelonglevel0/18/a.cpp
@@ -6,22 +6,23 @@ int i,j,k,flag,sum,counter;
 int a,b,t,q,l,r;
 
 vector <int> v(200001,0);
-
+multiset <pair<int,int> > ms;
 int main(){
     cin>>n>>k>>q;
     
     for(i=0;i<n;i++){
         cin>>l>>r;
         for(j=l;j<=r;j++){
-            v[j]++;
+            if(v[j]<k){
+                v[j]++;
+            }
         }
+        ms.insert({l,r});
     }
     for(i=0;i<q;i++){
         cin>>a>>b;
-        counter=0;
-        for(j=l;j<=r;j++){
-            v[j]++;
-        }
+        counter=count(v.begin()+a,v.begin()+b+1,k);
+        cout<<counter<<endl;
     }
     return 0;
 }
