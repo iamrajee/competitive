63d3174818d1d909bae0a0b4e1129c4af9dce92e
Rajendra Singh <singh.raj1997@gmail.com>
2019-07-20 21:33:07 +0530

    Update a.cpp

--- codeforces/competitions/CodeforcesGlobalRound4/a/a.cpp
+++ codeforces/competitions/CodeforcesGlobalRound4/a/a.cpp
@@ -2,42 +2,39 @@
 using namespace std;
 
 int n,m;
-int i,j,k,flag,sum,counter;
-int a,b,t,s;
-int T;
-multiset <int> mso;
-// multiset <int> mse;
+int i,j,k,flag,sum,count;
+int a,b,t;
+char c;
+string s;
+vector <int> v;
 int main(){
-    cin>>n>>k;
-    vector <int> v(k+1,0);    
-    sum=ceil((float)n/2);
-    counter=0;
-    // cout<<"sum:"<<sum<<endl;
-    for(i=0;i<n;i++){
+    cin>>n;
+    sum=0;
+    int sum2=0;
+    cin>>a;
+    sum2+=a;
+    v.push_back(1);
+    sum+=a;
+    for(i=1;i<n;i++){
         cin>>t;
-        v[t]++;
-        v.push_back(t);
-    }
-    for(i=0;i<k+1;i++){
-        if(v[i]!=0){
-            m=v[i]%2;
-            if(m==0){
-                sum-=v[i]/2;
-                counter+=v[i];
-            }else{
-                mso.insert(v[i]);
-                // cout<<v[i]<<endl;
-            }
-            
+        sum+=t;
+        if(a>=2*t){
+            //cout<<"hi"<<endl;
+            sum2+=t;
+            v.push_back(i+1);
         }
     }
-    // cout<<"sum:"<<sum<<endl;
-    for(auto it=mso.rbegin();it!=mso.rend() && sum!=0 ;it++){
-        counter+=min(*it,sum*2);
-        sum-=min((*it+1)/2,sum);
-        // cout<<*it<<", v[i]: "<<*it<<", sum: "<<sum<<endl;
+    //cout<<sum2<<sum<<endl;
+    if(sum2*2>sum){
+        int sz = (int)v.size();
+        cout<<sz<<endl;
+        cout<<v[0];
+        for(i=1;i<sz;i++){
+            cout<<" "<<v[i];
+        }
+        cout<<endl;
+    }else{
+        cout<<"0"<<endl;
     }
-
-    cout<<counter<<endl;
     return 0;
 }
