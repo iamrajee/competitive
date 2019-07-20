119f694c205aa373ff1d83bb4925f753a3d0e66e
Rajendra Singh <singh.raj1997@gmail.com>
2019-07-20 22:38:10 +0530

    Update a.cpp

--- codeforces/competitions/CodeforcesGlobalRound4/b/a.cpp
+++ codeforces/competitions/CodeforcesGlobalRound4/b/a.cpp
@@ -1,11 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-long long int n,m,k,c,r;
+int n,m;
+int i,j,k,flag,counter;
+long long int sum;
+int a,b,t;
+char c;
+string s;
+vector <int> v;
 int main(){
-    cin>>n>>k;
-    c = -2*(n+k);
-    r=(sqrt(9-4*c)-3)/2;
-    cout<<n-r<<endl;
+    cin>>s;
+    int sz=s.size();
+    sum=0;
+    counter=0;
+    for(i=1;i<sz;i++){
+        if(s[i]=='o'){
+            v.push_back(counter);        
+        }else if(s[i-1]=='v' && s[i]=='v' ){
+            counter++;
+        }
+    }
+    
+    for(auto it:v){
+        sum+=(it*(counter-it));
+    }
+    cout<<sum<<endl;
     return 0;
 }
