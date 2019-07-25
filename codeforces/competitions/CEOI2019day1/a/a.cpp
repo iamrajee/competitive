1326b6e019c77f0f9fd35122cebe1150b057e232
Rajendra Singh <singh.raj1997@gmail.com>
2019-07-26 02:03:52 +0530

    updated

--- codeforces/competitions/CEOI2019day1/a/a.cpp
+++ codeforces/competitions/CEOI2019day1/a/a.cpp
@@ -4,13 +4,15 @@ using namespace std;
 int n,m,q;
 int i,j,k,t,r,c;
 set <pair<int,int> > st;
-int f;
+set <pair<int,int> > st2;
+queue <pair<int,int> > qu;
 int main(){
     cin>>n;
     cin>>t;
     for(i=0;i<n;i++){
         cin>>r>>c;
         st.insert({r,c});
+        qu.push({r,c});
 
     }
 
@@ -23,9 +25,24 @@ int main(){
             return 0;
         }
     }
+
+    
     cout<<"YES"<<endl;
     if(t==1){
-        
+        r=qu.front().first;
+        c=qu.front().second;
+        st2.insert({r,c});
+        qu.pop();
+        while(qu.empty()!=1){
+            r=qu.front().first;
+            c=qu.front().second;
+            qu.pop();
+            if(st2.find({r+1,c+1})==st2.end() && st2.find({r-1,c-1})==st2.end() && st2.find({r+1,c-1})==st2.end() && st2.find({r-1,c+1})==st2.end() && st2.find({r+1,c})==st2.end() && st2.find({r-1,c})==st2.end() && st2.find({r,c+1})==st2.end() && st2.find({r,c-1})==st2.end()){
+                qu.push({r,c});
+            }else{
+                
+            }
+        }
     }else{
 
     }
